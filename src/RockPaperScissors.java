d554ab6ad8d45e6df32ac379c1e7b28d5c3dbb45
avianne7800 <avianne7800@gmail.com>
2024-10-10 16:26:27 -0400

    oops forgot to do a first commit

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+import java.util.Random;
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        boolean done = false;
+        String[] CPUPool = {"R","P","S"};
+        String CPUPlay;
+        String UserPlay;
+        int yesno;
+
+        do{
+            System.out.println("(R)ock, (P)aper, or (S)cissors?");
+            UserPlay = in.next();
+            in.nextLine();
+            int randomNumber = random.nextInt(CPUPool.length);
+            CPUPlay = CPUPool[randomNumber];
+            if(UserPlay.equalsIgnoreCase("R")){
+                switch (CPUPlay) {
+                    case "R" -> {
+                        System.out.println("P1: Rock");
+                        System.out.println("VS");
+                        System.out.println("CPU: Rock");
+                        System.out.println("It's a tie!");
+                    }
+                    case "P" -> {
+                        System.out.println("P1: Rock");
+                        System.out.println("VS");
+                        System.out.println("CPU: Paper");
+                        System.out.println("You lose!");
+                    }
+                    case "S" -> {
+                        System.out.println("P1: Rock");
+                        System.out.println("VS");
+                        System.out.println("CPU: Scissors");
+                        System.out.println("You win!");
+                    }
+                }
+            }
+            else if(UserPlay.equalsIgnoreCase("P")){
+                switch (CPUPlay) {
+                    case "R" -> {
+                        System.out.println("P1: Paper");
+                        System.out.println("VS");
+                        System.out.println("CPU: Rock");
+                        System.out.println("You win!");
+                    }
+                    case "P" -> {
+                        System.out.println("P1: Paper");
+                        System.out.println("VS");
+                        System.out.println("CPU: Paper");
+                        System.out.println("It's a tie!");
+                    }
+                    case "S" -> {
+                        System.out.println("P1: Paper");
+                        System.out.println("VS");
+                        System.out.println("CPU: Scissors");
+                        System.out.println("You lose!");
+                    }
+                }
+            }
+            else if(UserPlay.equalsIgnoreCase("S")){
+                switch (CPUPlay) {
+                    case "R" -> {
+                        System.out.println("P1: Scissors");
+                        System.out.println("VS");
+                        System.out.println("CPU: Rock");
+                        System.out.println("You lose!");
+                    }
+                    case "P" -> {
+                        System.out.println("P1: Scissors");
+                        System.out.println("VS");
+                        System.out.println("CPU: Paper");
+                        System.out.println("You win!");
+                    }
+                    case "S" -> {
+                        System.out.println("P1: Scissors");
+                        System.out.println("VS");
+                        System.out.println("CPU: Scissors");
+                        System.out.println("It's a tie!");
+                    }
+                }
+            }
+            else{
+                System.out.println("The input you entered is invalid");
+                UserPlay = "";
+            }
+        }while(!done);
+    }
+}
